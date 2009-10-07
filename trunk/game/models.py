2f61b8e32b6f831c5d466b6a435eae2c77e3522f
ghislain.leveque <ghislain.leveque@06bac804-b37a-11de-aad8-5f085193a258>
2009-10-07 20:03:19 +0000

    hop

--- trunk/game/models.py
+++ trunk/game/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 
-# Create your models here.
+def CardType(models.Model):
+	name = models.CharField()
+
 def Card(models.Model):
+	type = models.ForeignKey(CardType)
+	name = models.CharField()
+	lieu = models.ForeignKey(ManyCards)
+
+def Ressource(models.Model):
+	name = models.CharField()
+
+def Stock(models.Model):
+	ressource = models.ForeignKey(Ressource)
+	quantite  = models.IntegerField()
+	joueur    = models.ForeignKey(Joueur)
+
+def ManyCards(models.Model):
 	pass
+
+def Joueur(models.Model):
+	deck = models.ForeignKey(ManyCards)
+	main = models.ForeignKey(ManyCards)
+	cimetiere = models.ForeignKey(ManyCards)
+	table = models.ForeignKey(ManyCards)
+	vie = models.IntegerField()
+	name = models.CharField()
--- trunk/settings.py
+++ trunk/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/home/gl/src/card/card.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/home/celine/src/card/card.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -63,13 +63,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'card.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/gl/src/card/templates'
+    '/home/celine/src/card/templates'
 )
 
 INSTALLED_APPS = (
@@ -77,4 +77,5 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
 )
